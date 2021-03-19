ca64fc093f45b47ffaa0e9300a0c58889da739db
DieterHolvoet <dieterholvoet@3567222.no-reply.drupal.org>
2021-03-19 15:11:53 +0000

    Issue #3176679: Use MenuActiveTrail to get active trail status of menu items

--- gin_toolbar.post_update.php
+++ gin_toolbar.post_update.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Please update Gin to version 8.x-3.x-alpha32+ for new settings
+ * Please update Gin to version 8.x-3.x-alpha32+ for new settings.
  */
 function gin_toolbar_post_update_update_gin_alpha32() {
   // Empty update to cause a cache rebuild.
